1970c7b3e235a416f9a0fb77100d0cadf488765b
Wilson Ng <wilsonng234@gmail.com>
2023-11-21 14:52:13 +0800

    Create `RoundedInputField`

---
+++ app/src/main/java/com/example/android_chatbot/ui/components/RoundedInputField.kt
@@ -0,0 +1,61 @@
+package com.example.android_chatbot.ui.components
+
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.shape.RoundedCornerShape
+import androidx.compose.material3.ExperimentalMaterial3Api
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.OutlinedTextField
+import androidx.compose.material3.Surface
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import com.example.android_chatbot.ui.theme.AndroidChatBotTheme
+
+
+/**
+ * Composable that allows the user to input message
+ */
+@OptIn(ExperimentalMaterial3Api::class)
+@Composable
+fun RoundedInputField(
+    value: String,
+    onValueChange: (String) -> Unit,
+    onSendMessage: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    OutlinedTextField(
+        value = value,
+        onValueChange = onValueChange,
+        shape = RoundedCornerShape(50.dp),
+        modifier = modifier
+            .fillMaxWidth()
+            .padding(10.dp),
+    )
+}
+
+@Preview
+@Composable
+fun RoundedInputFieldPreview() {
+    AndroidChatBotTheme {
+        // A surface container using the 'background' color from the theme
+        Surface(
+            modifier = Modifier,
+            color = MaterialTheme.colorScheme.background
+        ) {
+            var prompt by remember { mutableStateOf("") }
+            val onSendMessage = {}
+
+            RoundedInputField(
+                value = prompt,
+                onValueChange = { prompt = it },
+                onSendMessage = onSendMessage
+            )
+        }
+    }
+}
