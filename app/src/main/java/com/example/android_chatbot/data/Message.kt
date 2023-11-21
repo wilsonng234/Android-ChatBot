bc93667f7513c44c7f4064246799e88126e7bf77
Wilson Ng <wilsonng234@gmail.com>
2023-11-21 23:40:31 +0800

    Annotate `Message` with `@Entity`

--- app/src/main/java/com/example/android_chatbot/data/Message.kt
+++ app/src/main/java/com/example/android_chatbot/data/Message.kt
@@ -1,6 +1,23 @@
 package com.example.android_chatbot.data
 
-data class Message(val role: String, val content: String) {
+import androidx.room.ColumnInfo
+import androidx.room.Entity
+import androidx.room.PrimaryKey
+
+@Entity(tableName = "message")
+data class Message(
+    @PrimaryKey(autoGenerate = true)
+    val id: Int = 0,
+
+    @ColumnInfo(name = "role")
+    val role: String,
+
+    @ColumnInfo(name = "content")
+    val content: String,
+
+    @ColumnInfo(name = "created_time")
+    val createdTime: Long
+) {
     override fun toString(): String {
         return """
             {
