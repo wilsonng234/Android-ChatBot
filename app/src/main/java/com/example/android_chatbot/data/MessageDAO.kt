a807f8e67eabfef87197f0562791316942781843
Wilson Ng <wilsonng234@gmail.com>
2023-11-21 23:43:15 +0800

    Create `MessageDAO`

---
+++ app/src/main/java/com/example/android_chatbot/data/MessageDAO.kt
@@ -0,0 +1,15 @@
+package com.example.android_chatbot.data
+
+import androidx.room.Dao
+import androidx.room.Insert
+import androidx.room.OnConflictStrategy
+import androidx.room.Query
+
+@Dao
+interface MessageDAO {
+    @Insert(onConflict = OnConflictStrategy.ABORT)
+    fun insertAll(vararg messages: Message)
+
+    @Query("SELECT * FROM message")
+    fun getAll(): List<Message>
+}
