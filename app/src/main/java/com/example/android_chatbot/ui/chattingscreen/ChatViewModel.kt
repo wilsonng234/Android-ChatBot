456bcff19b3c7a139ef772dda96062d31c682926
JamesLIKwokHo <109494293+JamesLIKwokHo@users.noreply.github.com>
2023-11-26 23:20:17 +0800

    `getChatResponse` failed in `ChatViewModel`

--- app/src/main/java/com/example/android_chatbot/ui/chattingscreen/ChatViewModel.kt
+++ app/src/main/java/com/example/android_chatbot/ui/chattingscreen/ChatViewModel.kt
@@ -1,8 +1,10 @@
 package com.example.android_chatbot.ui.chattingscreen
 
+import android.util.Log
 import androidx.compose.runtime.mutableStateListOf
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.viewModelScope
+import com.example.android_chatbot.model.azure.AzureOpenAIService
 import kotlinx.coroutines.launch
 
 class ChatViewModel : ViewModel() {
@@ -12,7 +14,10 @@ class ChatViewModel : ViewModel() {
         messages.add(Message(text, "user"))
         if (isUser) {
             viewModelScope.launch {
-//                val response = messages
+                val service = AzureOpenAIService("ea86fbb837a84230aa8acb2993eae139", "https://hkust.azure-api.net/openai/deployments/gpt-35-turbo/chat/completions?api-version=2023-05-15")
+                val response = service.getChatResponse(messages.toList())
+//                val format = "{\"messages\":${messages.toList().joinToString(prefix = "[", postfix = "]")}}"
+//                Log.d("TAG", format)
             }
         }
     }
@@ -29,4 +34,13 @@ class ChatViewModel : ViewModel() {
 data class Message(val content: String, val role: String) {
     val isUser: Boolean
         get() = role == "user"
+
+    override fun toString(): String {
+        return """
+            {
+                "role": "$role",
+                "content": "$content"
+            }
+        """.trimIndent()
+    }
 }
