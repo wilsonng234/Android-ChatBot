e4c55cc6a2d5ed7707c0cbc4e003c9980e2a1ad6
Wilson Ng <wilsonng234@gmail.com>
2023-11-27 23:18:56 +0800

    Replace input text field with `RoundedInputField`

--- app/src/main/java/com/example/android_chatbot/ui/chattingscreen/ChattingScreen.kt
+++ app/src/main/java/com/example/android_chatbot/ui/chattingscreen/ChattingScreen.kt
@@ -1,25 +1,16 @@
 package com.example.android_chatbot.ui.chattingscreen
 
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.wrapContentWidth
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
-import androidx.compose.foundation.text.KeyboardActions
-import androidx.compose.foundation.text.KeyboardOptions
-import androidx.compose.material.icons.Icons
-import androidx.compose.material.icons.filled.Send
 import androidx.compose.material3.ExperimentalMaterial3Api
-import androidx.compose.material3.Icon
-import androidx.compose.material3.IconButton
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Surface
 import androidx.compose.material3.Text
-import androidx.compose.material3.TextField
-import androidx.compose.material3.TextFieldDefaults
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -27,10 +18,10 @@ import androidx.compose.runtime.remember
 import androidx.compose.runtime.setValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.text.input.ImeAction
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.lifecycle.viewmodel.compose.viewModel
+import com.example.android_chatbot.ui.components.RoundedInputField
 
 
 @OptIn(ExperimentalMaterial3Api::class)
@@ -51,35 +42,12 @@ fun ChattingScreen(viewModel: ChatViewModel = viewModel(), modifier: Modifier = 
             }
         }
 
-        Row(
-            modifier = Modifier
-                .fillMaxWidth()
-                .padding(16.dp)
-        ) {
-            var inputText by remember { mutableStateOf("") }
-            TextField(
-                value = inputText,
-                onValueChange = { inputText = it },
-                modifier = Modifier.weight(1f),
-                colors = TextFieldDefaults.textFieldColors(),
-                label = { Text("Type a message") },
-                singleLine = true,
-                keyboardOptions = KeyboardOptions.Default.copy(imeAction = ImeAction.Send),
-                keyboardActions = KeyboardActions(onSend = {
-                    viewModel.sendMessage(inputText)
-                    inputText = ""
-                })
-            )
-
-            IconButton(
-                onClick = {
-                    viewModel.sendMessage(inputText)
-                    inputText = ""
-                }, modifier = Modifier.padding(start = 8.dp)
-            ) {
-                Icon(Icons.Default.Send, contentDescription = "Send message")
-            }
-        }
+        var inputPrompt by remember { mutableStateOf("") }
+        RoundedInputField(inputPrompt, onValueChange = { inputPrompt = it }, onSendMessage = {
+            viewModel.sendMessage(inputPrompt)
+            inputPrompt = ""
+        }, modifier = modifier
+        )
     }
 }
 
