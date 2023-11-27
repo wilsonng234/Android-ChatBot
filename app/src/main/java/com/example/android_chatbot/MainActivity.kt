e31019a3ebbf225aa40033b9ab340c321415693b
Wilson Ng <wilsonng234@gmail.com>
2023-11-27 23:44:23 +0800

    Remove `ChattingPreview`

--- app/src/main/java/com/example/android_chatbot/MainActivity.kt
+++ app/src/main/java/com/example/android_chatbot/MainActivity.kt
@@ -3,10 +3,6 @@ package com.example.android_chatbot
 import android.os.Bundle
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
-import androidx.compose.runtime.Composable
-import androidx.compose.ui.tooling.preview.Preview
-import androidx.lifecycle.viewmodel.compose.viewModel
-import com.example.android_chatbot.ui.chattingscreen.ChattingViewModel
 import com.example.android_chatbot.ui.chattingscreen.ChattingScreen
 import com.example.android_chatbot.ui.theme.AndroidChatBotTheme
 
@@ -18,16 +14,5 @@ class MainActivity : ComponentActivity() {
                 ChattingScreen()
             }
         }
-    }
-}
-
-@Preview(showBackground = true)
-@Composable
-fun ChattingPreview() {
-    AndroidChatBotTheme {
-        val chattingViewModel = viewModel<ChattingViewModel>()
-        ChattingScreen(chattingViewModel)
-        chattingViewModel.printMessage("Hi", true)
-        chattingViewModel.printMessage("Hello! How can I assist you today?", false)
     }
 }
