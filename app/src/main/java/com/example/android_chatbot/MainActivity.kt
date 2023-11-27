adfdb7b805f12924996065b1e505015f98f9ede1
Wilson Ng <wilsonng234@gmail.com>
2023-11-28 00:01:12 +0800

    Add `channel` parameter to `ChattingViewModel`

--- app/src/main/java/com/example/android_chatbot/MainActivity.kt
+++ app/src/main/java/com/example/android_chatbot/MainActivity.kt
@@ -11,7 +11,7 @@ class MainActivity : ComponentActivity() {
         super.onCreate(savedInstanceState)
         setContent {
             AndroidChatBotTheme {
-                ChattingScreen()
+                ChattingScreen("channel")
             }
         }
     }
