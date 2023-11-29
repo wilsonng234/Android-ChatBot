846aabcb64e26bb57db91a4fcaa8d9c409fdbb9b
Wilson Ng <wilsonng234@gmail.com>
2023-11-29 19:22:16 +0800

    Integrate tables as one database

---
+++ app/src/main/java/com/example/android_chatbot/data/Database.kt
@@ -0,0 +1,36 @@
+package com.example.android_chatbot.data
+
+import android.content.Context
+import androidx.room.Database
+import androidx.room.Room
+import androidx.room.RoomDatabase
+import com.example.android_chatbot.data.channel.Channel
+import com.example.android_chatbot.data.channel.ChannelDAO
+import com.example.android_chatbot.data.message.Message
+import com.example.android_chatbot.data.message.MessageDAO
+import com.example.android_chatbot.data.setting.Setting
+import com.example.android_chatbot.data.setting.SettingDAO
+
+@Database(entities = [Channel::class, Message::class, Setting::class], version = 1)
+abstract class ChatbotDatabase : RoomDatabase() {
+    abstract fun channelDAO(): ChannelDAO
+    abstract fun messageDAO(): MessageDAO
+    abstract fun settingDAO(): SettingDAO
+
+    companion object {
+        @Volatile
+        private lateinit var INSTANCE: ChatbotDatabase
+
+        fun getDatabase(context: Context): ChatbotDatabase {
+            synchronized(this) {
+                if (!Companion::INSTANCE.isInitialized) {
+                    INSTANCE = Room.databaseBuilder(
+                        context.applicationContext, ChatbotDatabase::class.java, "channel.db"
+                    ).build()
+                }
+
+                return INSTANCE
+            }
+        }
+    }
+}
