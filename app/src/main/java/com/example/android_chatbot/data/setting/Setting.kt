92a998d635035ed05dae83aed5f2e7ee6f5b2230
Wilson Ng <wilsonng234@gmail.com>
2023-12-01 20:32:09 +0800

    Rename `category` as `service`

--- app/src/main/java/com/example/android_chatbot/data/setting/Setting.kt
+++ app/src/main/java/com/example/android_chatbot/data/setting/Setting.kt
@@ -5,11 +5,11 @@ import androidx.room.Entity
 import androidx.room.Index
 import androidx.room.PrimaryKey
 
-@Entity(tableName = "setting", indices = [Index(value = ["category"], unique = true)])
+@Entity(tableName = "setting", indices = [Index(value = ["service"], unique = true)])
 data class Setting(
     @PrimaryKey(autoGenerate = true) val id: Int = 0,
 
     @ColumnInfo(name = "api_key") val apiKey: String,
 
-    @ColumnInfo(name = "category") val category: String,
+    @ColumnInfo(name = "service") val service: String,
 )
