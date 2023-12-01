702ffbf887f7a7219b25a847bdb70df92f608721
Wilson Ng <wilsonng234@gmail.com>
2023-12-01 20:09:52 +0800

    Remove repository

--- app/src/main/java/com/example/android_chatbot/MainActivity.kt
+++ app/src/main/java/com/example/android_chatbot/MainActivity.kt
@@ -1,22 +1,32 @@
 package com.example.android_chatbot
 
 import android.os.Bundle
+import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import com.example.android_chatbot.data.ChatBotApplication
-import com.example.android_chatbot.data.channel.ChannelRepository
-import com.example.android_chatbot.data.message.MessageRepository
-import com.example.android_chatbot.data.setting.SettingRepository
+import com.example.android_chatbot.data.channel.ChannelDAO
+import com.example.android_chatbot.data.message.MessageDAO
+import com.example.android_chatbot.data.setting.SettingDAO
 import com.example.android_chatbot.ui.chattingscreen.ChattingScreen
 import com.example.android_chatbot.ui.theme.AndroidChatBotTheme
+import kotlinx.coroutines.CoroutineScope
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.async
 
 class MainActivity : ComponentActivity() {
-    private val channelRepository: ChannelRepository by lazy { (application as ChatBotApplication).channelRepository }
-    private val messageRepository: MessageRepository by lazy { (application as ChatBotApplication).messageRepository }
-    private val settingRepository: SettingRepository by lazy { (application as ChatBotApplication).settingRepository }
+    private val channelDAO: ChannelDAO by lazy { (application as ChatBotApplication).channelDAO }
+    private val messageDAO: MessageDAO by lazy { (application as ChatBotApplication).messageDAO }
+    private val settingDAO: SettingDAO by lazy { (application as ChatBotApplication).settingDAO }
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+
+        CoroutineScope(Dispatchers.IO).async {
+            Log.d("onCreate", "onCreate: ${channelDAO.getAll()}")
+            Log.d("onCreate", "onCreate: ${messageDAO.getAll()}")
+            Log.d("onCreate", "onCreate: ${settingDAO.getAll()}")
+        }
 
         setContent {
             AndroidChatBotTheme {
--- app/src/main/java/com/example/android_chatbot/data/ChatBotApplication.kt
+++ app/src/main/java/com/example/android_chatbot/data/ChatBotApplication.kt
@@ -1,14 +1,11 @@
 package com.example.android_chatbot.data
 
 import android.app.Application
-import com.example.android_chatbot.data.channel.ChannelRepository
-import com.example.android_chatbot.data.message.MessageRepository
-import com.example.android_chatbot.data.setting.SettingRepository
 
 class ChatBotApplication : Application() {
     private val chatBotDatabase by lazy { ChatBotDatabase.getDatabase(this) }
 
-    val channelRepository by lazy { ChannelRepository(chatBotDatabase.channelDAO()) }
-    val messageRepository by lazy { MessageRepository(chatBotDatabase.messageDAO()) }
-    val settingRepository by lazy { SettingRepository(chatBotDatabase.settingDAO()) }
+    val channelDAO by lazy { chatBotDatabase.channelDAO() }
+    val messageDAO by lazy { chatBotDatabase.messageDAO() }
+    val settingDAO by lazy { chatBotDatabase.settingDAO() }
 }
