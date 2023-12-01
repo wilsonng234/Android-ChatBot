a4b7f68f52b22dbdd45396af782ef042baf615d7
Wilson Ng <wilsonng234@gmail.com>
2023-12-02 03:59:02 +0800

    Add `handleMenuItemClicked` to `MenuItemCard`

--- app/src/main/java/com/example/android_chatbot/ui/components/MenuItemCard.kt
+++ app/src/main/java/com/example/android_chatbot/ui/components/MenuItemCard.kt
@@ -1,14 +1,17 @@
 package com.example.android_chatbot.ui.components
 
+import androidx.compose.foundation.background
+import androidx.compose.foundation.border
+import androidx.compose.foundation.clickable
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Row
-import androidx.compose.foundation.layout.Spacer
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.material3.Icon
+import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
@@ -18,14 +21,24 @@ import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 
 @Composable
-fun MenuItemCard(icon: ImageVector, content: String, modifier: Modifier = Modifier) {
-    Row(
-        verticalAlignment = Alignment.CenterVertically,
+fun MenuItemCard(
+    icon: ImageVector,
+    content: String,
+    handleMenuItemClicked: () -> Unit,
+    modifier: Modifier = Modifier
+) {
+    Row(verticalAlignment = Alignment.CenterVertically,
         horizontalArrangement = Arrangement.Start,
-        modifier = modifier.fillMaxSize()
-    ) {
-        Icon(imageVector = icon, contentDescription = "Menu Icon")
-        Spacer(modifier = modifier)
+        modifier = modifier
+            .fillMaxSize()
+            .border(10.dp, color = MaterialTheme.colorScheme.background)
+            .background(color = MaterialTheme.colorScheme.background)
+            .clickable { handleMenuItemClicked() }) {
+        Icon(
+            imageVector = icon,
+            contentDescription = "Menu Icon",
+            modifier.padding(horizontal = 10.dp)
+        )
         Text(text = content)
     }
 }
@@ -36,8 +49,7 @@ fun MenuItemCardPreview() {
     MenuItemCard(
         icon = Icons.Filled.Menu,
         content = "Menu Item",
-        modifier = Modifier
-            .height(60.dp)
-            .padding(10.dp)
+        handleMenuItemClicked = {},
+        modifier = Modifier.height(60.dp)
     )
 }
