12322ba8f5210d8171cd31bc6b11dad434958ffc
Wilson Ng <wilsonng234@gmail.com>
2023-12-02 20:19:24 +0800

    Extract value state

--- app/src/main/java/com/example/android_chatbot/ui/components/FormInputField.kt
+++ app/src/main/java/com/example/android_chatbot/ui/components/FormInputField.kt
@@ -4,26 +4,21 @@ import androidx.compose.material3.ExperimentalMaterial3Api
 import androidx.compose.material3.OutlinedTextField
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
-import androidx.compose.runtime.getValue
-import androidx.compose.runtime.mutableStateOf
-import androidx.compose.runtime.saveable.rememberSaveable
-import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
 
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun FormInputField(
+    value: String,
+    onValueChange: (String) -> Unit,
     label: String,
-    initialValue: String = "",
     singleLine: Boolean = true,
     modifier: Modifier = Modifier
 ) {
-    var value by rememberSaveable { mutableStateOf(initialValue) }
-
     OutlinedTextField(
         value = value,
-        onValueChange = { value = it },
+        onValueChange = { onValueChange },
         modifier = modifier,
         label = { Text(label) },
         singleLine = singleLine,
@@ -33,5 +28,5 @@ fun FormInputField(
 @Preview
 @Composable
 fun FormInputFieldPreview() {
-    FormInputField(label = "label", initialValue = "initialValue")
+    FormInputField(value = "value", onValueChange = {}, label = "label")
 }
