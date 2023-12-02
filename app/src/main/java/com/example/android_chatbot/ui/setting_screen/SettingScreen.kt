0e70b1916fe9e13911dfbac519b2f3c4c6639664
Wilson Ng <wilsonng234@gmail.com>
2023-12-02 20:20:49 +0800

    Use `FormInputField`

--- app/src/main/java/com/example/android_chatbot/ui/setting_screen/SettingScreen.kt
+++ app/src/main/java/com/example/android_chatbot/ui/setting_screen/SettingScreen.kt
@@ -1,13 +1,32 @@
 package com.example.android_chatbot.ui.setting_screen
 
-import androidx.compose.material3.Text
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.padding
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+import com.example.android_chatbot.data.DataSource
 import com.example.android_chatbot.data.setting.SettingDAO
+import com.example.android_chatbot.ui.components.FormInputField
 
 @Composable
 fun SettingScreen(
     settingDAO: SettingDAO, modifier: Modifier = Modifier
 ) {
-    Text("Setting Screen")
+    Column(modifier = modifier) {
+        for (service in DataSource.services) {
+            var apiKey by remember { mutableStateOf("") }
+
+            FormInputField(
+                value = apiKey,
+                onValueChange = { apiKey = it },
+                label = service,
+                modifier = Modifier.padding(4.dp)
+            )
+        }
+    }
 }
