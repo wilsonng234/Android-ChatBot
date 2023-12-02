b37028ef7d83166d4de031fc1c24730ccf89a80a
Wilson Ng <wilsonng234@gmail.com>
2023-12-02 13:15:10 +0800

    Change directory of `ChatBotApplication`

--- app/src/main/java/com/example/android_chatbot/ChatBotApplication.kt
+++ app/src/main/java/com/example/android_chatbot/ChatBotApplication.kt
@@ -1,6 +1,7 @@
-package com.example.android_chatbot.data
+package com.example.android_chatbot
 
 import android.app.Application
+import com.example.android_chatbot.data.ChatBotDatabase
 
 class ChatBotApplication : Application() {
     private val chatBotDatabase by lazy { ChatBotDatabase.getDatabase(this) }
