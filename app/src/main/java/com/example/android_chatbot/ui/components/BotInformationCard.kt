b253595f02622bd15cfe05a3da53ebf4bb637182
Wilson Ng <wilsonng234@gmail.com>
2023-12-04 02:26:13 +0800

    Improve `SelectBotScreen` layout

--- app/src/main/java/com/example/android_chatbot/ui/components/BotInformationCard.kt
+++ app/src/main/java/com/example/android_chatbot/ui/components/BotInformationCard.kt
@@ -2,23 +2,22 @@ package com.example.android_chatbot.ui.components
 
 import androidx.compose.foundation.Image
 import androidx.compose.foundation.clickable
-import androidx.compose.foundation.layout.Box
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
-import androidx.compose.foundation.layout.wrapContentSize
 import androidx.compose.material3.MaterialTheme
 import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.text.font.FontWeight
+import androidx.compose.ui.text.style.TextOverflow
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
-import androidx.compose.ui.unit.sp
 import com.example.android_chatbot.R
 
 @Composable
@@ -30,41 +29,40 @@ fun BotInformationCard(
     handleOnClicked: () -> Unit,
     modifier: Modifier
 ) {
-    Box(
-        modifier
-            .wrapContentSize()
-            .clickable { handleOnClicked() },
-    ) {
-        Row(
-            modifier.fillMaxWidth(),
-            verticalAlignment = Alignment.CenterVertically,
+    Row(verticalAlignment = Alignment.CenterVertically,
+        horizontalArrangement = Arrangement.SpaceEvenly,
+        modifier = modifier
+            .height(100.dp)
+            .fillMaxWidth()
+            .clickable { handleOnClicked() }) {
+        Image(
+            painter = painterResource(id = iconId),
+            contentDescription = iconId.toString(),
+            modifier = modifier
+                .padding(12.dp)
+                .weight(0.2f)
+        )
+
+        Column(
+            modifier = modifier
+                .padding(8.dp)
+                .weight(0.8f)
         ) {
-            Image(
-                painter = painterResource(id = iconId),
-                contentDescription = contentDescription,
+            Text(
+                text = service,
+                style = MaterialTheme.typography.titleLarge,
+                color = MaterialTheme.colorScheme.onBackground,
+                fontWeight = FontWeight.Bold,
                 modifier = modifier
-                    .padding(12.dp)
-                    .weight(0.2f)
             )
-            Column(
-                modifier = modifier
-                    .padding(8.dp)
-                    .padding(top = 12.dp)
-                    .weight(0.8f)
-            ) {
-                Text(
-                    text = service,
-                    fontSize = 24.sp,
-                    style = MaterialTheme.typography.titleLarge,
-                )
-                Text(
-                    text = model,
-                    fontSize = 18.sp,
-                    style = MaterialTheme.typography.bodyMedium,
-                    color = Color.Black.copy(alpha = 0.5f),
-                    modifier = modifier.padding(top = 4.dp)
-                )
-            }
+
+            Text(
+                text = model,
+                style = MaterialTheme.typography.bodyMedium,
+                color = MaterialTheme.colorScheme.outline,
+                overflow = TextOverflow.Ellipsis,
+                modifier = modifier.padding(top = 4.dp)
+            )
         }
     }
 }
--- app/src/main/java/com/example/android_chatbot/ui/select_bot_screen/SelectBotScreen.kt
+++ app/src/main/java/com/example/android_chatbot/ui/select_bot_screen/SelectBotScreen.kt
@@ -1,12 +1,11 @@
 package com.example.android_chatbot.ui.select_bot_screen
 
-import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.unit.dp
 import com.example.android_chatbot.R
 import com.example.android_chatbot.data.DataSource.servicesToModels
@@ -23,37 +22,47 @@ import kotlinx.coroutines.withContext
 fun SelectBotScreen(
     channelDAO: ChannelDAO, handleChatRoomClicked: (Long) -> Unit, modifier: Modifier = Modifier
 ) {
-    LazyColumn() {
-        for (entry in servicesToModels.entries.iterator()) {
-            val iconId = if (entry.key == "Azure OpenAI") {
-                R.drawable.azure
-            } else if (entry.key == "OpenAI") {
-                R.drawable.openai
-            } else {
-                throw IllegalStateException("Unknown service")
-            }
-            items(entry.value.size) {
-                BotInformationCard(
-                    iconId = iconId,
-                    service = entry.key,
-                    model = entry.value[it],
-                    handleOnClicked = {
-                        CoroutineScope(Dispatchers.IO).launch {
-                            val channelIds = channelDAO.insertAll(
-                                Channel(
-                                    service = entry.key, model = entry.value[it]
-                                )
-                            )
-                            val channelId = channelIds[0]
+    Column {
+        Divider(
+            modifier = modifier
+                .padding(top = 8.dp)
+                .padding(horizontal = 8.dp)
+        )
 
-                            withContext(Dispatchers.Main) {
-                                handleChatRoomClicked(channelId)
+        LazyColumn() {
+            for (entry in servicesToModels.entries.iterator()) {
+                val iconId = if (entry.key == "Azure OpenAI") {
+                    R.drawable.azure
+                } else if (entry.key == "OpenAI") {
+                    R.drawable.openai
+                } else {
+                    throw IllegalStateException("Unknown service")
+                }
+
+                items(entry.value.size) {
+                    BotInformationCard(
+                        iconId = iconId,
+                        service = entry.key,
+                        model = entry.value[it],
+                        handleOnClicked = {
+                            CoroutineScope(Dispatchers.IO).launch {
+                                val channelIds = channelDAO.insertAll(
+                                    Channel(
+                                        service = entry.key, model = entry.value[it]
+                                    )
+                                )
+                                val channelId = channelIds[0]
+
+                                withContext(Dispatchers.Main) {
+                                    handleChatRoomClicked(channelId)
+                                }
                             }
-                        }
-                    },
-                    modifier = modifier.height(125.dp)
-                )
-                Divider(color = Color.LightGray, modifier = Modifier.padding(horizontal = 8.dp))
+                        },
+                        modifier = modifier
+                    )
+
+                    Divider(modifier = modifier.padding(horizontal = 8.dp))
+                }
             }
         }
     }
