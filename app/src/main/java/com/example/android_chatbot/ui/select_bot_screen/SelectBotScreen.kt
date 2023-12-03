4a4509e00408b4b6a571e80b5aabb015e94f5f95
JamesLIKwokHo <109494293+JamesLIKwokHo@users.noreply.github.com>
2023-12-03 20:59:19 +0800

    `SelectBotScreen` fetch data from `DataSource`

--- app/src/main/java/com/example/android_chatbot/ui/select_bot_screen/SelectBotScreen.kt
+++ app/src/main/java/com/example/android_chatbot/ui/select_bot_screen/SelectBotScreen.kt
@@ -1,11 +1,16 @@
 package com.example.android_chatbot.ui.select_bot_screen
 
+import android.util.Log
+import androidx.compose.foundation.layout.PaddingValues
 import androidx.compose.foundation.layout.height
+import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.Menu
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
+import com.example.android_chatbot.data.DataSource
+import com.example.android_chatbot.data.DataSource.servicesToModels
 import com.example.android_chatbot.data.channel.Channel
 import com.example.android_chatbot.data.channel.ChannelDAO
 import com.example.android_chatbot.ui.components.BotInformationCard
@@ -19,20 +24,26 @@ import kotlinx.coroutines.withContext
 fun SelectBotScreen(
     channelDAO: ChannelDAO, handleChatRoomClicked: (Long) -> Unit, modifier: Modifier = Modifier
 ) {
-    BotInformationCard(
-        icon = Icons.Filled.Menu,
-        service = "Azure OpenAI",
-        description = "Hello! How can I assist you today?",
-        handleOnClicked = {
-            CoroutineScope(Dispatchers.IO).launch {
-                val channelIds = channelDAO.insertAll(Channel(service = "Azure OpenAI"))
-                val channelId = channelIds[0]
+    LazyColumn(contentPadding = PaddingValues(16.dp)) {
+        for (entry in servicesToModels.entries.iterator()) {
+            items(entry.value.size) {
+                BotInformationCard(
+                    icon = Icons.Filled.Menu,
+                    service = entry.key,
+                    description = entry.value[it],
+                    handleOnClicked = {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val channelIds = channelDAO.insertAll(Channel(service = entry.key))
+                            val channelId = channelIds[0]
 
-                withContext(Dispatchers.Main) {
-                    handleChatRoomClicked(channelId)
-                }
+                            withContext(Dispatchers.Main) {
+                                handleChatRoomClicked(channelId)
+                            }
+                        }
+                    },
+                    modifier = modifier.height(150.dp)
+                )
             }
-        },
-        modifier = modifier.height(150.dp)
-    )
+        }
+    }
 }
