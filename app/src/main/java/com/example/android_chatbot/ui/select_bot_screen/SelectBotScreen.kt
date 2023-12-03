6a5dcd0f4506e817c7d76cf80fcd72fd249cf728
Wilson Ng <wilsonng234@gmail.com>
2023-12-04 03:01:27 +0800

    Refactor `SelectBotScreen`

--- app/src/main/java/com/example/android_chatbot/ui/select_bot_screen/SelectBotScreen.kt
+++ app/src/main/java/com/example/android_chatbot/ui/select_bot_screen/SelectBotScreen.kt
@@ -3,8 +3,14 @@ package com.example.android_chatbot.ui.select_bot_screen
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.material3.Divider
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.LaunchedEffect
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.unit.dp
 import com.example.android_chatbot.R
@@ -18,10 +24,42 @@ import kotlinx.coroutines.launch
 import kotlinx.coroutines.withContext
 
 
+data class BotInformation(
+    val iconId: Int, val service: String, val model: String
+)
+
 @Composable
 fun SelectBotScreen(
     channelDAO: ChannelDAO, handleChatRoomClicked: (Long) -> Unit, modifier: Modifier = Modifier
 ) {
+    var botInformationList by remember { mutableStateOf<List<BotInformation>>(emptyList()) }
+
+    LaunchedEffect(Unit) {
+        val temp = botInformationList.map { it }.toMutableList()
+
+        for (entry in servicesToModels.entries.iterator()) {
+            val iconId = when (entry.key) {
+                "Azure OpenAI" -> {
+                    R.drawable.azure
+                }
+
+                "OpenAI" -> {
+                    R.drawable.openai
+                }
+
+                else -> {
+                    throw IllegalStateException("Unknown service")
+                }
+            }
+
+            for (model in entry.value) {
+                temp.add(BotInformation(iconId, entry.key, model))
+            }
+        }
+
+        botInformationList = temp
+    }
+
     Column {
         Divider(
             modifier = modifier
@@ -29,40 +67,31 @@ fun SelectBotScreen(
                 .padding(horizontal = 8.dp)
         )
 
-        LazyColumn() {
-            for (entry in servicesToModels.entries.iterator()) {
-                val iconId = if (entry.key == "Azure OpenAI") {
-                    R.drawable.azure
-                } else if (entry.key == "OpenAI") {
-                    R.drawable.openai
-                } else {
-                    throw IllegalStateException("Unknown service")
-                }
-
-                items(entry.value.size) {
-                    BotInformationCard(
-                        iconId = iconId,
-                        service = entry.key,
-                        model = entry.value[it],
-                        handleOnClicked = {
-                            CoroutineScope(Dispatchers.IO).launch {
-                                val channelIds = channelDAO.insertAll(
-                                    Channel(
-                                        service = entry.key, model = entry.value[it]
-                                    )
+        LazyColumn {
+            items(botInformationList) {
+                BotInformationCard(
+                    iconId = it.iconId,
+                    contentDescription = null,
+                    service = it.service,
+                    model = it.model,
+                    handleOnClicked = {
+                        CoroutineScope(Dispatchers.IO).launch {
+                            val channelIds = channelDAO.insertAll(
+                                Channel(
+                                    service = it.service, model = it.model
                                 )
-                                val channelId = channelIds[0]
+                            )
+                            val channelId = channelIds[0]
 
-                                withContext(Dispatchers.Main) {
-                                    handleChatRoomClicked(channelId)
-                                }
+                            withContext(Dispatchers.Main) {
+                                handleChatRoomClicked(channelId)
                             }
-                        },
-                        modifier = modifier
-                    )
+                        }
+                    },
+                    modifier = modifier
+                )
 
-                    Divider(modifier = modifier.padding(horizontal = 8.dp))
-                }
+                Divider(modifier = modifier.padding(horizontal = 8.dp))
             }
         }
     }
