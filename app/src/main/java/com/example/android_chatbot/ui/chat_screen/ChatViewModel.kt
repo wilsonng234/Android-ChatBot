7421f80314a020c3462feaca08c383806b968f62
Wilson Ng <wilsonng234@gmail.com>
2023-12-03 20:41:43 +0800

    Remove unused functions

--- app/src/main/java/com/example/android_chatbot/ui/chat_screen/ChatViewModel.kt
+++ app/src/main/java/com/example/android_chatbot/ui/chat_screen/ChatViewModel.kt
@@ -2,13 +2,9 @@ package com.example.android_chatbot.ui.chat_screen
 
 import androidx.lifecycle.ViewModel
 import androidx.lifecycle.ViewModelProvider
-import androidx.lifecycle.viewModelScope
-import com.example.android_chatbot.data.message.Message
 import com.example.android_chatbot.data.message.MessageDAO
 import com.example.android_chatbot.data.setting.SettingDAO
 import com.example.android_chatbot.model.azure.AzureOpenAIService
-import kotlinx.coroutines.Dispatchers
-import kotlinx.coroutines.launch
 
 //
 class ChatViewModel(
@@ -27,36 +23,5 @@ class ChatViewModel(
 
     init {
         AzureOpenAIService.init(settingDAO)
-    }
-
-    fun insertMessage(message: Message) {
-        viewModelScope.launch(Dispatchers.IO) {
-            messageDAO.insertAll(message)
-        }
-    }
-
-    fun sendMessage(text: String, channelMessages: List<Message>) {
-        insertMessage(
-            Message(
-                channelId = channelId,
-                role = "user",
-                content = text,
-                createdTime = System.currentTimeMillis()
-            )
-        )
-
-        viewModelScope.launch {
-            val response = AzureOpenAIService.getChatResponse(channelMessages)
-            if (response.second) {
-                insertMessage(
-                    Message(
-                        channelId = channelId,
-                        role = "assistant",
-                        content = response.first,
-                        createdTime = System.currentTimeMillis()
-                    )
-                )
-            }
-        }
     }
 }
