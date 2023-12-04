0e22c27f4de7c27e4b39e3fbee7556f0396421d5
Wilson Ng <wilsonng234@gmail.com>
2023-12-04 11:36:15 +0800

    Change `ChatHistoryCard` height at start screen

--- app/src/main/java/com/example/android_chatbot/ui/StartScreen.kt
+++ app/src/main/java/com/example/android_chatbot/ui/StartScreen.kt
@@ -3,6 +3,7 @@ package com.example.android_chatbot.ui
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.lazy.LazyColumn
 import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
@@ -11,6 +12,7 @@ import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
 import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
 import com.example.android_chatbot.R
 import com.example.android_chatbot.data.channel.ChannelDAO
 import com.example.android_chatbot.data.message.MessageDAO
@@ -67,6 +69,7 @@ fun StartScreen(
                     recentChat = recentChat,
                     time = time,
                     onClick = onClick,
+                    cardHeight = 110,
                     modifier = modifier
                 )
             }
