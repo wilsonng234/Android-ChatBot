1ea7720e0d29f476d318976b8941db3b79f63a14
Wilson Ng <wilsonng234@gmail.com>
2023-12-04 10:41:29 +0800

    Make start screen scrollable with LazyColumn

--- app/src/main/java/com/example/android_chatbot/ui/StartScreen.kt
+++ app/src/main/java/com/example/android_chatbot/ui/StartScreen.kt
@@ -1,17 +1,21 @@
 package com.example.android_chatbot.ui
 
+import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
-import androidx.compose.foundation.layout.padding
-import androidx.compose.material3.Divider
+import androidx.compose.foundation.layout.fillMaxHeight
+import androidx.compose.foundation.lazy.LazyColumn
+import androidx.compose.foundation.lazy.items
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.unit.dp
 import com.example.android_chatbot.R
 import com.example.android_chatbot.data.channel.ChannelDAO
 import com.example.android_chatbot.data.message.MessageDAO
 import com.example.android_chatbot.ui.components.ChatHistoryCard
+import com.example.android_chatbot.ui.components.RoundedInputField
 
 @Composable
 fun StartScreen(
@@ -21,42 +25,46 @@ fun StartScreen(
     modifier: Modifier = Modifier
 ) {
     val channels by channelDAO.getAll().collectAsState(initial = emptyList())
+    val (inputPrompt, setInputPrompt) = remember { mutableStateOf("") }
 
-    Column(modifier = modifier) {
-        Divider(
-            modifier = modifier
-                .padding(top = 8.dp)
-        )
+    Column(verticalArrangement = Arrangement.SpaceBetween, modifier = modifier) {
+        LazyColumn(modifier = modifier.fillMaxHeight(0.85f)) {
+            items(channels) { channel ->
+                val messages by messageDAO.getMessagesByChannelId(channel.id)
+                    .collectAsState(initial = emptyList())
+                val lastMessage by messageDAO.getLastMessageByChannelId(channel.id).collectAsState(
+                    initial = null
+                )
 
-        for (channel in channels) {
-            val messages by messageDAO.getMessagesByChannelId(channel.id)
-                .collectAsState(initial = emptyList())
-            val lastMessage = messages.lastOrNull()
-            val ser = when (channel.service) {
-                "Azure OpenAI" -> {
-                    R.drawable.azure
+                val ser = when (channel.service) {
+                    "Azure OpenAI" -> {
+                        R.drawable.azure
+                    }
+
+                    "OpenAI" -> {
+                        R.drawable.openai
+                    }
+
+                    else -> {
+                        throw IllegalStateException("Unknown service")
+                    }
                 }
 
-                "OpenAI" -> {
-                    R.drawable.openai
-                }
+                val recentChat = lastMessage?.content ?: ""
+                val time = lastMessage?.createdTime
 
-                else -> {
-                    throw IllegalStateException("Unknown service")
-                }
+                ChatHistoryCard(
+                    iconId = ser,
+                    channelId = channel.id,
+                    service = channel.service,
+                    model = channel.model,
+                    topic = channel.topic,
+                    recentChat = recentChat,
+                    time = time,
+                    onClick = onClick,
+                    modifier = modifier
+                )
             }
-
-            ChatHistoryCard(
-                iconId = ser,
-                channelId = channel.id,
-                service = channel.service,
-                model = channel.model,
-                topic = channel.topic,
-                recentChat = lastMessage?.content ?: "",
-                time = lastMessage?.createdTime,
-                onClick = onClick,
-                modifier = modifier
-            )
         }
     }
 }
