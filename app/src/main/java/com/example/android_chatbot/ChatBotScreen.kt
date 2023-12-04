77dbcb3d8cc89fc36fe7cbebcd4b3c85f06e937d
casuljt <108253711+casuljt@users.noreply.github.com>
2023-12-04 16:12:48 +0800

    topbar update

--- app/src/main/java/com/example/android_chatbot/ChatBotScreen.kt
+++ app/src/main/java/com/example/android_chatbot/ChatBotScreen.kt
@@ -3,6 +3,7 @@ package com.example.android_chatbot
 import androidx.annotation.StringRes
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
@@ -12,7 +13,6 @@ import androidx.compose.foundation.verticalScroll
 import androidx.compose.material.icons.Icons
 import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.material.icons.filled.Menu
-import androidx.compose.material.icons.outlined.Face
 import androidx.compose.material.icons.outlined.Person
 import androidx.compose.material.icons.outlined.Settings
 import androidx.compose.material3.Divider
@@ -32,6 +32,7 @@ import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.rememberCoroutineScope
+import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.res.stringResource
 import androidx.compose.ui.unit.dp
@@ -42,6 +43,7 @@ import androidx.navigation.compose.composable
 import androidx.navigation.compose.currentBackStackEntryAsState
 import androidx.navigation.compose.rememberNavController
 import androidx.navigation.navArgument
+import com.example.android_chatbot.data.channel.Channel
 import com.example.android_chatbot.data.channel.ChannelDAO
 import com.example.android_chatbot.data.message.MessageDAO
 import com.example.android_chatbot.data.setting.SettingDAO
@@ -54,7 +56,7 @@ import com.example.android_chatbot.ui.setting_screen.SettingScreen
 import kotlinx.coroutines.launch
 
 enum class ChatBotScreen(@StringRes val title: Int) {
-    Start(title = R.string.app_name), AllChats(title = R.string.all_chats), SelectBot(title = R.string.select_bot), Settings(
+    Start(title = R.string.app_name), SelectBot(title = R.string.select_bot), Settings(
         title = R.string.settings
     ),
     Chat(title = R.string.chat)
@@ -63,6 +65,7 @@ enum class ChatBotScreen(@StringRes val title: Int) {
 @OptIn(ExperimentalMaterial3Api::class)
 @Composable
 fun ChatBotTopAppBar(
+    channel: Channel?,
     currentScreen: String,
     canNavigateBack: Boolean,
     handleNavigationIconClicked: (Boolean) -> () -> Unit = { {} },
@@ -71,14 +74,33 @@ fun ChatBotTopAppBar(
     TopAppBar(title = { Text("") }, colors = mediumTopAppBarColors(
         containerColor = MaterialTheme.colorScheme.primaryContainer
     ), modifier = modifier, navigationIcon = {
-        IconButton(onClick = handleNavigationIconClicked(canNavigateBack)) {
-            Icon(
-                imageVector = if (!canNavigateBack) Icons.Filled.Menu else Icons.Filled.ArrowBack,
-                contentDescription = if (!canNavigateBack) stringResource(R.string.drawer_button) else stringResource(
-                    R.string.back_button
+        Row{
+            IconButton(onClick = handleNavigationIconClicked(canNavigateBack)) {
+                Icon(
+                    imageVector = if (!canNavigateBack) Icons.Filled.Menu else Icons.Filled.ArrowBack,
+                    contentDescription = if (!canNavigateBack) stringResource(R.string.drawer_button) else stringResource(
+                        R.string.back_button
+                    )
                 )
-            )
+            }
+            Column(Modifier.align(Alignment.CenterVertically)) {
+                if(currentScreen == ChatBotScreen.Start.name){
+                    Text("PayAsYouGo")
+                }else if(currentScreen == ChatBotScreen.Settings.name){
+                    Text("Setting")
+                }else if(currentScreen == ChatBotScreen.SelectBot.name){
+                    Text("Select Your partner to chat today!")
+                }else if (currentScreen.contains(ChatBotScreen.Chat.name)){
+                    if (channel != null) {
+                        Text(channel.topic)
+                        Text(channel.model)
+                    }
+
+                }
+            }
+
         }
+
     })
 }
 
@@ -128,10 +150,6 @@ fun ChatBotApp(
         }
 
         when (menuItemId) {
-            ChatBotScreen.AllChats.title -> {
-                navHostController.navigate(ChatBotScreen.AllChats.name)
-            }
-
             ChatBotScreen.SelectBot.title -> {
                 navHostController.navigate(ChatBotScreen.SelectBot.name)
             }
@@ -147,7 +165,7 @@ fun ChatBotApp(
     }
 
     val menuItemIds = listOf(
-        ChatBotScreen.AllChats.title, ChatBotScreen.SelectBot.title, ChatBotScreen.Settings.title
+        ChatBotScreen.SelectBot.title, ChatBotScreen.Settings.title
     )
 
     ModalNavigationDrawer(drawerState = drawerState, drawerContent = {
@@ -194,7 +212,6 @@ fun ChatBotApp(
                 menuItemIds.map { menuItemId ->
                     MenuItemCard(
                         icon = when (menuItemId) {
-                            ChatBotScreen.AllChats.title -> Icons.Outlined.Face
                             ChatBotScreen.SelectBot.title -> Icons.Outlined.Person
                             ChatBotScreen.Settings.title -> Icons.Outlined.Settings
 
@@ -213,7 +230,12 @@ fun ChatBotApp(
         }
     }) {
         Scaffold(topBar = {
-            ChatBotTopAppBar(currentScreen = currentScreen,
+            val currentChannelId = backStackEntry?.arguments?.getLong("channelId") ?: null
+            val currentChannel = if(currentChannelId != null){channelDAO.getChannelById(currentChannelId)}
+                                 else{null}
+            ChatBotTopAppBar(
+                channel = currentChannel,
+                currentScreen = currentScreen,
                 canNavigateBack = navHostController.previousBackStackEntry != null,
                 handleNavigationIconClicked = { handleNavigationIconClicked(it) })
         }) { paddingValues ->
@@ -230,9 +252,6 @@ fun ChatBotApp(
                         StartScreen(channelDAO = channelDAO,
                             messageDAO = messageDAO,
                             onClick = { handleChatCardClicked(it) })
-                    }
-                    composable(route = ChatBotScreen.AllChats.name) {
-                        Text("AllChats Screen")
                     }
                     composable(route = ChatBotScreen.SelectBot.name) {
                         SelectBotScreen(channelDAO = channelDAO, handleChatRoomClicked = {
