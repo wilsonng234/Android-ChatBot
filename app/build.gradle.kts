e88f9860022ded6fa5c78ab03346b01228fc0c1d
Wilson Ng <wilsonng234@gmail.com>
2023-12-05 15:19:05 +0800

    Upgrade bom version

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -100,7 +100,7 @@ dependencies {
 
     androidTestImplementation("androidx.test.ext:junit:1.1.5")
     androidTestImplementation("androidx.test.espresso:espresso-core:3.5.1")
-    androidTestImplementation(platform("androidx.compose:compose-bom:2023.03.00"))
+    androidTestImplementation(platform("androidx.compose:compose-bom:2023.10.01"))
     androidTestImplementation("androidx.compose.ui:ui-test-junit4")
 
     debugImplementation("androidx.compose.ui:ui-tooling")
